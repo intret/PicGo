092c0c2794ef19c6af01476a1abce0d0cb22b8ec
Zilllch <intret@qq.com>
2017-07-30 00:55:59 +0800

    正确照片拍照时间与今天的间隔天数

--- app/src/main/java/cn/intret/app/picgo/utils/DateTimeUtils.java
+++ app/src/main/java/cn/intret/app/picgo/utils/DateTimeUtils.java
@@ -2,33 +2,41 @@ package cn.intret.app.picgo.utils;
 
 import android.content.res.Resources;
 
-import org.apache.commons.lang3.time.DateUtils;
 import org.joda.time.DateTime;
-import org.joda.time.Duration;
-import org.joda.time.Minutes;
 import org.joda.time.Period;
 import org.joda.time.PeriodType;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
 
 import cn.intret.app.picgo.R;
 
 public class DateTimeUtils {
 
-    public static int daysFromToday(Date date) {
-
-        DateTime end = new DateTime(date);
+    /**
+     * 计算一个日期在今天之前的天数
+     * @param date 指定要计算的日期
+     * @return 返回一个正数，表示指定的日期与今天的距离天数，0 表示今天，1表示昨天，2表示前天；返回一个负数，表示今天与指定日期的天数距离。
+     */
+    public static int daysBeforeToday(Date date) {
 
         DateTime now = DateTime.now();
-        //计算区间天数
-        Period p = new Period(end, now, PeriodType.days());
-        return p.getDays()+1;
+        if (date.after(now.toDate())) {
+            DateTime start = DateTime.now().dayOfYear().roundFloorCopy(); // 今天的开始时间
+            DateTime end = new DateTime(date);
+
+            Period p = new Period(start, end, PeriodType.days());
+            return -p.getDays();
+        } else {
+            DateTime start = new DateTime(date);
+            DateTime end = now.dayOfYear().roundCeilingCopy(); // 今天的结束时间
+            return new Period(start, end, PeriodType.days()).getDays();
+        }
     }
 
-    public static int daysFromToday(long date) {
-        return daysFromToday(new Date(date));
+    public static int daysBeforeToday(long date) {
+        return daysBeforeToday(new Date(date));
     }
 
     public static String friendlyDayDescription(Resources res, Date date) {
@@ -36,10 +44,10 @@ public class DateTimeUtils {
     }
 
     public static String friendlyDayDescription(Resources res, long date) {
-        int i = daysFromToday(date);
-        if (i == 0) {
+        int days = daysBeforeToday(date);
+        if (days == 0) {
             return res.getString(R.string.today);
-        } else if (i == 1) {
+        } else if (days == 1) {
             return res.getString(R.string.yesterdy);
         } else {
             return new SimpleDateFormat("yyyy-MM-dd").format(new Date(date));
